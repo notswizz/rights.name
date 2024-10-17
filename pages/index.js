905aa7d5e1814d098ac4c4bd0b827e2f10a43783
Jack Smith <jsmith@ISE-JSmith.local>
2024-10-17 01:32:56 -0400

    awsesome sauce 2

--- pages/index.js
+++ pages/index.js
@@ -10,15 +10,18 @@ export default function Home() {
       
       {/* Landing Page Section */}
       <div className="container mx-auto px-4 py-16 text-center">
-        <div className="mb-8">
-          <Image
-            src="/ise.jpeg"
-            alt="ISE Logo"
-            width={200}
-            height={200}
-            className="mx-auto rounded-full shadow-lg"
-          />
-        </div>
+      <div className="mb-8">
+  <div className="relative mx-auto w-52 h-52 rounded-full shadow-2xl overflow-hidden transform transition duration-500 hover:scale-105 hover:shadow-xl">
+    <Image
+      src="/isecover.jpeg"
+      alt="ISE Cover"
+      layout="fill"
+      objectFit="cover"
+      className="rounded-full"
+    />
+  </div>
+</div>
+  
         <h1 className="text-4xl md:text-6xl font-bold mb-4 text-gray-800">
           Independent Sports & Entertainment
         </h1>
@@ -26,12 +29,18 @@ export default function Home() {
           The Leader in College Naming Rights 
         </p>
         <div className="flex justify-center space-x-4">
-          <Link href="/research" className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out transform hover:scale-105">
-            Explore Our Data
-          </Link>
-          <Link href="/chat" className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out transform hover:scale-105">
-            Chat With Bot
-          </Link>
+          <div className="text-center">
+            <Link href="/research" className="inline-block bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-800 text-white font-bold py-6 px-6 rounded-full shadow-lg transform transition duration-300 ease-in-out hover:scale-110 hover:shadow-xl">
+              Explore Our Data
+            </Link>
+           
+          </div>
+          <div className="text-center">
+            <Link href="/chat" className="inline-block bg-gradient-to-r from-green-500 to-green-700 hover:from-green-600 hover:to-green-800 text-white font-bold py-6 px-6 rounded-full shadow-lg transform transition duration-300 ease-in-out hover:scale-110 hover:shadow-xl">
+              Chat With Bot
+            </Link>
+           
+          </div>
         </div>
       </div>
     </div>
